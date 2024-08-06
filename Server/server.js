89d0c4759efdaa76375b4d43b3b4cd4d390960fe
TheCodster <168859142+KushalXCoder@users.noreply.github.com>
2024-08-06 16:31:14 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,90 +1,3 @@
-// const { MongoClient } = require("mongodb");
-// const express = require("express");
-// const axios = require("axios");
-// const uniqid = require("uniqid");
-// const sha256 = require("sha256");
-// const session = require("express-session");
-// const jwt = require("jsonwebtoken");
-// const dotenv = require("dotenv");
-// const path = require("path");
-// const cookieParser = require("cookie-parser");
-// const cors = require("cors");
-// const nodemailer = require("nodemailer");
-
-// dotenv.config({ path: path.join(__dirname, "../ENV Files/.env") });
-
-
-// const app = express();
-// const port = 3000;
-// const SERVER_URL = "https://los-santos-elite-1.onrender.com/";
-
-// const HOST_URL = process.env.HOST_URL;
-// const MERCHANT_ID = process.env.MERCHANT_ID;
-// const SALT_INDEX = process.env.SALT_INDEX;
-// const SALT_KEY = process.env.SALT_KEY;
-// const EMAIL_USERNAME = process.env.EMAIL_USERNAME;
-// const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
-// const PAYMENT_DATA = [];
-
-// const testProperties = {};
-
-// const secretKey = process.env.SESSION_SECRET_KEY;
-
-// app.use(session({
-// 	secret: secretKey,
-// 	resave: false,
-// 	saveUninitialized: false,
-// 	cookie: { secure: false, sameSite: true }
-// }));
-
-// app.use(cors({
-// 	origin: ['http://127.0.0.1:5500', "https://los-santos-elite-1.onrender.com/"],
-// 	credentials: true
-// }));
-
-// app.use(cookieParser());
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }))
-
-// const uri = process.env.DATABASE_URI;
-
-// const client = new MongoClient(uri);
-
-// const Building_Details = client.db("Feature_Testing").collection("Building Details");
-
-// const Usernames_and_Passwords = client.db("Feature_Testing").collection("Usernames and Passwords");
-
-// const Car_Details = client.db("Feature_Testing").collection("Car Details");
-
-// const transporter = nodemailer.createTransport({
-// 	host: "smtp.gmail.com",
-// 	auth: {
-// 		user: EMAIL_USERNAME,
-// 		pass: EMAIL_PASSWORD
-// 	}
-// });
-
-// transporter.verify((error, success) => {
-// 	if (error) {
-// 		console.log(error);
-// 	} else {
-// 		console.log("Done!!");
-// 		console.log(success);
-// 	}
-// })
-
-// async function main() {
-// 	try {
-// 		await client.connect();
-// 		console.log("Connected successfully to MongoDB");
-// 	} catch (error) {
-// 		console.error("Error connecting to MongoDB: ", error);
-// 	}
-// }
-
-// main().catch(console.error);
-
 const { MongoClient } = require("mongodb");
 const express = require("express");
 const axios = require("axios");
@@ -99,6 +12,7 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 dotenv.config({ path: path.join(__dirname, "../ENV Files/.env") });
+
 
 const app = express();
 const port = 3000;
@@ -117,76 +31,59 @@ const testProperties = {};
 const secretKey = process.env.SESSION_SECRET_KEY;
 
 app.use(session({
-    secret: secretKey,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false, sameSite: true }
+	secret: secretKey,
+	resave: false,
+	saveUninitialized: false,
+	cookie: { secure: false, sameSite: true }
 }));
 
 app.use(cors({
-    origin: (origin, callback) => {
-        console.log("Origin: ", origin);
-        if (!origin || ['http://127.0.0.1:5500', 'https://los-santos-elite-bbb4.vercel.app'].indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+	origin: ['http://127.0.0.1:5500', "https://los-santos-elite-1.onrender.com"],
+	credentials: true
 }));
 
-app.options('*', cors()); // Enable preflight for all routes
-
 app.use(cookieParser());
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 const uri = process.env.DATABASE_URI;
 
 const client = new MongoClient(uri);
 
 const Building_Details = client.db("Feature_Testing").collection("Building Details");
+
 const Usernames_and_Passwords = client.db("Feature_Testing").collection("Usernames and Passwords");
+
 const Car_Details = client.db("Feature_Testing").collection("Car Details");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    auth: {
-        user: EMAIL_USERNAME,
-        pass: EMAIL_PASSWORD
-    }
+	host: "smtp.gmail.com",
+	auth: {
+		user: EMAIL_USERNAME,
+		pass: EMAIL_PASSWORD
+	}
 });
 
 transporter.verify((error, success) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Done!!");
-        console.log(success);
-    }
-});
+	if (error) {
+		console.log(error);
+	} else {
+		console.log("Done!!");
+		console.log(success);
+	}
+})
 
 async function main() {
-    try {
-        await client.connect();
-        console.log("Connected successfully to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB: ", error);
-    }
+	try {
+		await client.connect();
+		console.log("Connected successfully to MongoDB");
+	} catch (error) {
+		console.error("Error connecting to MongoDB: ", error);
+	}
 }
 
 main().catch(console.error);
-
-app.post('/add-new-user', (req, res) => {
-    res.json({ message: 'User added successfully' });
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
 
 async function getPropertyDetails(query) {
 	const result = await Building_Details.find(query).toArray();
