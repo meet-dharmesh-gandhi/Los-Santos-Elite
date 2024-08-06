7e6d4f9445b06a9f6e8aea7a9944cdc9a282d62f
TheCodster <168859142+KushalXCoder@users.noreply.github.com>
2024-08-06 14:55:56 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -16,7 +16,7 @@ dotenv.config({ path: path.join(__dirname, "../ENV Files/.env") });
 
 const app = express();
 const port = 3000;
-const SERVER_URL = "https://los-santos-elite-1.onrender.com/";
+const SERVER_URL = "https://los-santos-elite-bbb4.vercel.app";
 
 const HOST_URL = process.env.HOST_URL;
 const MERCHANT_ID = process.env.MERCHANT_ID;
@@ -38,7 +38,7 @@ app.use(session({
 }));
 
 app.use(cors({
-	origin: ['http://127.0.0.1:5500', "https://los-santos-elite-bbb4.vercel.app/"],
+	origin: ['http://127.0.0.1:5500', "https://los-santos-elite-bbb4.vercel.app"],
 	credentials: true
 }));
 
